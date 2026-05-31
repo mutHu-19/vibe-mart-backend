b591b014bde9e11876fd49afbc81244e5a34ae36
mutHu-19 <arundathi.muthukumari@gmail.com>
2026-05-31 16:24:52 +0530

    server js redo

--- server.js
+++ server.js
@@ -5,18 +5,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: function(origin, callback) {
-    const allowed = [
-      'https://vibe-mart-topaz.vercel.app',
-      'http://localhost:3000',
-    ];
-    // Allow requests with no origin (mobile apps, curl, Postman)
-    if (!origin || allowed.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true
 }));
 app.use(express.json());
