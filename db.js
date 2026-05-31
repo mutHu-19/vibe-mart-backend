2350388b8ea1c1ff7f08941a015afd742f4d53d5
mutHu-19 <arundathi.muthukumari@gmail.com>
2026-05-31 16:31:23 +0530

    db update

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 const mysql = require('mysql2/promise');
 
-const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
-  database: process.env.DB_NAME || 'shoplk',
+  database: process.env.DB_NAME || 'railway',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   waitForConnections: true,
