c5989c1e8aa1db919a3724b69bb6f321bf7c1413
mutHu-19 <arundathi.muthukumari@gmail.com>
2026-05-31 16:06:59 +0530

    second

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2/promise');
 
+const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
